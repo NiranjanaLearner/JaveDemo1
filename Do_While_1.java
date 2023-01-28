8e0043dda65faa55c198be50c38b2087c46d761b
Niranjana <niranjanalearner@gmail.com>
2023-01-28 23:14:57 +0530

    third commit

---
+++ Do_While_1.java
@@ -0,0 +1,16 @@
+package Packageuesd1;
+
+public class Do_While_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		do
+		{
+			System.out.println("inside do loop"+num);
+			num--;
+		}while(num>0);
+		
+	}
+
+}
