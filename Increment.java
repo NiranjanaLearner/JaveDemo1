e286fb8998eef9569269a53ec3287f8191a04175
Niranjana <niranjanalearner@gmail.com>
2023-01-28 21:53:41 +0530

    First commit

---
+++ Increment.java
@@ -0,0 +1,14 @@
+package Packageuesd1;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=6;
+		int b=8;
+		System.out.println(++a);
+		System.out.println(b++);
+		System.out.println(b);
+	}
+
+}
